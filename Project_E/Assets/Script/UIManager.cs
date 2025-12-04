17d3add1bbf5f2efab67b86ef2dbef7a9da655a8
CH-22222 <cndgnl79@gmail.com>
2025-12-04 14:34:59 +0900

    251204ignore

--- Project_E/Assets/Script/UIManager.cs
+++ Project_E/Assets/Script/UIManager.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject popupPrefab;
-    public Transform canvasTransform;
-    public string TitleText;
+    public GameObject popupPrefab; //스폰해줄 프리팹 변수
+    public Transform canvasTransform; //어느 위치에서 스폰할지
+    public string TitleText; // 어느 팝업을 띄울지
     public string ContentText;
 
     public void OpenNoticePopup()
     {
-        GameObject newPopup = Instantiate(popupPrefab, canvasTransform);
-        Popup popupScript = newPopup.GetComponent<Popup>();
+        GameObject newPopup = Instantiate(popupPrefab, canvasTransform);// 생성한 팝업 변수 (스폰하면서 newPopup 변수에 집어넣음)
+        Popup popupScript = newPopup.GetComponent<Popup>();// 팝업 스크립트 변수
 
-        popupScript.SetContent(this.TitleText, this.ContentText);
+        popupScript.SetContent(this.TitleText, this.ContentText);// 팝업 내용 설정
     }
 }
